a6cfb8006b3049a71fc0af5e06c24ff94d28512e
bugden.peter@hotmail.com <bugden.peter@hotmail.com>
2018-12-12 23:44:59 -0500

    added a facebook share button

--- app/src/main/java/com/example/abdallah/AndroidRestaurant/ShareActivity.java
+++ app/src/main/java/com/example/abdallah/AndroidRestaurant/ShareActivity.java
@@ -89,5 +89,15 @@ public class ShareActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        findViewById(R.id.btnFacebook).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.facebook.com/share.php?u="));
+                startActivity(intent);
+            }
+        });
     }
 }
